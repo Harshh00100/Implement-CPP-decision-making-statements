f666d585bd7ce0cd9f27a0e5dc896ef216c65ba0
Harsh Kumar <kunarharsh76@gmail.com>
2025-07-18 19:43:24 +0530

    Add files via upload

---
+++ EXP5-d.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b ;
+    int c;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Calculator Menu" <<"\n";
+    cout << "Enter 1 for Addition"<< "\n";
+    cout << "Enter 2 for Subtraction" << "\n";
+    cout << "Enter 3 for Multiplication" << "\n";
+    cout << "Enter 4 for Division" << "\n";
+    cin >> c;
+    switch(c) {
+        case 1:
+            cout << "Result: " << a + b ;
+            break;
+        case 2:
+            cout << "Result: " << a - b ;
+            break;
+        case 3:
+            cout << "Result: " << a * b ;
+            break;
+        case 4:
+            if (b != 0) {
+                cout << "Result: " << a/ b;
+            } else {    
+                cout << "not defined" << "\n";
+            }
+            break;
+        default:
+            cout << "Enter Valid option";
+    }
+    return 0;
+}
