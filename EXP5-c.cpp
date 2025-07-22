d947744f86a4e5b7888713050d04596c2221bca6
Harsh Kumar <kunarharsh76@gmail.com>
2025-07-22 14:16:57 +0530

    Update EXP5-c.cpp

--- EXP5-c.cpp
+++ EXP5-c.cpp
@@ -3,32 +3,55 @@ using namespace std;
 
 int main() {
     int choice;
-    cout<<"enter day no. 1-7 of week \n" ;//menu
+    cout<<"enter month 1-12 of the year \n" ;//menu
     cout<<"Enter your choice: ";
     cin >> choice;
     switch(choice) { //switch case
         case 1:
-        cout<<"The day is Monday ";
+        cout<<"The month is january ";
         break;
         case 2:
-        cout<<"The day is Tuesday ";
+        cout<<"The month is february ";
         break;
         case 3:
-        cout<<"The day is Wednesday ";
+        cout<<"The month is march";
         break;
         case 4:
-        cout<<"The day is Thursday ";
+        cout<<"The month is april";
         break;
         case 5:
-        cout<<"The day is Friday ";
+        cout<<"The month is may";
         break;
         case 6:
-        cout<<"The day is Saturday ";
+        cout<<"The month is june ";
         break;
         case 7:
-        cout<<"The day is Sunday ";
+        cout<<"The month is july";
+        break;
+         case 8:
+        cout<<"The month is August";
+        break;
+        case 9:
+        cout<<"The month is september";
+        break;
+         case 10:
+        cout<<"The month is october";
+        break;
+         case 11:
+        cout<<"The month is november";
+        break;
+         case 12:
+        cout<<"The month is december";
         break;
         default:
         cout<<"INVALID CHOICE!!!!";//default statement
     }
 }
+
+
+
+/* output :
+enter month 1-12 of the year 
+Enter your choice: 5
+The month is may
+*/
